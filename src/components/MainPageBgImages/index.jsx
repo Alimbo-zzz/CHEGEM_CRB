212fa0d58b3aa15ce07e9c45363a6ea594d15218
Alim <56486795+Alimbo-zzz@users.noreply.github.com>
2023-04-21 20:33:27 +0300

    blue theme

--- src/components/MainPageBgImages/index.jsx
+++ src/components/MainPageBgImages/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import scss from './styles.module.scss';
 import bg_1 from '@images/bg_4.png';
 import bg_2 from '@images/bg_3.png';
+import bg_3 from '@images/bg_1.png';
+import bg_4 from '@images/bg_2.png';
 
 const MainPageBgImages = (events) => {
 	const {} = events;
@@ -9,7 +11,7 @@ const MainPageBgImages = (events) => {
 	return (<>
 		<div className={scss.wrap}>			
 			{/* <img src={bg_2} className={scss.bg_1}/> */}
-			<img src={bg_1} className={scss.bg_2}/>
+			<img src={bg_4} className={scss.bg_2}/>
 			{/* <img src={bg_2} className={scss.bg_3}/> */}
 		</div>
 	</>);
